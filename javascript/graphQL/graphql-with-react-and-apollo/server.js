6eaf770406e208e2642bef295b3fb636e97ee390
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-01-28 17:08:35 +1100

    Mission UI update for SpaceX Launch

--- javascript/graphQL/graphql-with-react-and-apollo/server.js
+++ javascript/graphQL/graphql-with-react-and-apollo/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-// const express = require("express");
+const cors = require("cors");
 
 const schema = require("./schema");
 
 const app = express();
+
+// Allow cros-origin
+app.use(cors());
 
 app.use(
   "/graphql",
@@ -14,6 +17,6 @@ app.use(
   })
 );
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started at ${PORT}`));
