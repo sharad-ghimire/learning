3a69d93c239fec7eb1fa47bc88d4f0bcbb1f78ef
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-02-04 19:14:11 +1100

    [Dev-connector] Comments route

--- javascript/MERN/devconnector/routes/api/posts.js
+++ javascript/MERN/devconnector/routes/api/posts.js
@@ -144,4 +144,63 @@ router.post(
   }
 );
 
+// @route         POST api/posts/comment/:id -> post id
+// @description   Add comment to post
+// @access        protected
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    // For Validation
+    const { errors, isValid } = validatePostInput(req.body);
+    if (!isValid) return res.status(400).json(errors);
+
+    Post.findById(req.params.id)
+      .then((post) => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+
+        // Add to comments array
+        post.comments.unshift(newComment);
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
+// @route         DELETE api/posts/comment/:id/:comment_id -> post id
+// @description   Delete comment from post
+// @access        protected
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then((post) => {
+        // Check if comment exits
+        if (
+          post.comments.filter(
+            (comment) => comment._id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res.status(404).json({ commentnotfound: 'No comment found' });
+        }
+
+        // Get the removeIndex
+        const removeIndex = post.comments
+          .map((item) => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //Splice comment out of array
+        post.comments.splice(removeIndex, 1);
+        post.save().then((post) => res.json(post));
+      })
+      .catch((err) => res.status(404).json({ postnotfound: 'No post found' }));
+  }
+);
+
 module.exports = router;
