15f98e159e07e3cf4dea14ec9e726dd9fe85b9e5
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-02-05 20:24:44 +1100

    [Dev-connector] Login and Register validation in frontend

--- javascript/MERN/devconnector/client/src/components/auth/Login.js
+++ javascript/MERN/devconnector/client/src/components/auth/Login.js
@@ -1,10 +1,69 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    };
+  }
+  onChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = (e) => {
+    e.preventDefault();
+    const loggedUser = {
+      email: this.state.email,
+      password: this.state.password
+    };
+    console.log(loggedUser);
+  };
+
   render() {
     return (
       <div>
-        <h1>Login</h1>
+        <div>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-8 m-auto'>
+                <h1 className='display-4 text-center'>Log In</h1>
+                <p className='lead text-center'>
+                  Log in to you DevConnector Account
+                </p>
+                <form onSubmit={this.onSubmit}>
+                  <div className='form-group'>
+                    <input
+                      type='email'
+                      className='form-control form-control-lg'
+                      name='email'
+                      placeholder='Email Address'
+                      value={this.state.email}
+                      onChange={this.onChange}
+                    />
+                  </div>
+                  <div className='form-group'>
+                    <input
+                      type='password'
+                      className='form-control form-control-lg'
+                      name='password'
+                      placeholder='Password'
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    />
+                  </div>
+
+                  <input
+                    type='submit'
+                    className='btn btn-info btn-block mt-4'
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- javascript/MERN/devconnector/client/src/components/auth/Register.js
+++ javascript/MERN/devconnector/client/src/components/auth/Register.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import classnames from 'classnames';
 
 class Register extends Component {
   constructor() {
@@ -25,10 +27,20 @@ class Register extends Component {
       password: this.state.password,
       password2: this.state.password2
     };
-    console.log(newUser);
+
+    // We dont have to do http://localhost:5000 because we added it as proxy value in package.json
+    // We want all of those dsone through Redux actions
+
+    axios
+      .post('/api/users/register', newUser)
+      .then((result) => console.log(result.data))
+      .catch((err) => this.setState({ errors: err.response.data }));
   };
 
   render() {
+    // Pulls errors object from state
+    const { errors } = this.state;
+
     return (
       <div>
         <div className='container'>
@@ -42,18 +54,25 @@ class Register extends Component {
                 <div className='form-group'>
                   <input
                     type='text'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.name
+                    })}
                     name='name'
                     placeholder='Name'
                     value={this.state.name}
                     onChange={this.onChange}
                   />
+                  {errors.name && (
+                    <div className='invalid-feedback'>{errors.name}</div>
+                  )}
                 </div>
 
                 <div className='form-group'>
                   <input
                     type='email'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.email
+                    })}
                     name='email'
                     placeholder='Email Address'
                     value={this.state.email}
@@ -63,26 +82,39 @@ class Register extends Component {
                     This site uses Gravatar so if you wan a profile image, use a
                     Gravatar email.
                   </small>
+                  {errors.email && (
+                    <div className='invalid-feedback'>{errors.email}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='password'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password
+                    })}
                     name='password'
                     placeholder='Password'
                     value={this.state.password}
                     onChange={this.onChange}
                   />
+                  {errors.password && (
+                    <div className='invalid-feedback'>{errors.password}</div>
+                  )}
                 </div>
                 <div className='form-group'>
                   <input
                     type='password'
-                    className='form-control form-control-lg'
+                    className={classnames('form-control form-control-lg', {
+                      'is-invalid': errors.password2
+                    })}
                     name='password2'
                     placeholder='Confirm Password'
                     value={this.state.password2}
                     onChange={this.onChange}
                   />
+                  {errors.password2 && (
+                    <div className='invalid-feedback'>{errors.password2}</div>
+                  )}
                 </div>
                 <input type='submit' className='btn btn-info btn-block mt-4' />
               </form>
