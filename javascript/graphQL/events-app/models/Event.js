35cbcd3b716c4b8d7049c2a0a9f5757fad9c6753
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-02-20 23:54:23 +1100

    [Event-app] User and Event Relations with resolver

--- javascript/graphQL/events-app/models/Event.js
+++ javascript/graphQL/events-app/models/Event.js
@@ -5,7 +5,8 @@ const EventSchema = new Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   price: { type: Number, required: true },
-  date: { type: Date, required: true }
+  date: { type: Date, required: true },
+  creator: { type: Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('Event', EventSchema);
