75658168d00a6f3f19d4e92811264a268397247f
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-02-21 13:42:25 +1100

    [Event-app] Login Query with JWT Web Token

--- javascript/graphQL/events-app/graphql/resolvers/auth.js
+++ javascript/graphQL/events-app/graphql/resolvers/auth.js
@@ -1,7 +1,30 @@
-const User = require('../../models/User.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../../models/User.js');
 
 module.exports = {
+  login: async ({ email, password }) => {
+    try {
+    } catch (error) {
+      throw error;
+    }
+
+    // Validate email and password combination is correct
+    const user = await User.findOne({ email });
+    if (!user) throw new Error('User doesnot exist');
+    const isEqual = await bcrypt.compare(password, user.password);
+    if (!isEqual) throw new Error('Password Incorrect');
+    // Create Token
+    const token = await jwt.sign(
+      { userId: user.id, email: user.email },
+      'someKey',
+      {
+        expiresIn: '1h'
+      }
+    );
+    // Should meet the requirement of AuthDat in schema
+    return { userId: user.id, token, tokenExpiration: 1 };
+  },
   createUser: async ({ input }) => {
     try {
       const user = await User.findOne({ email: input.email });
--- javascript/graphQL/events-app/graphql/schema/index.js
+++ javascript/graphQL/events-app/graphql/schema/index.js
@@ -25,6 +25,12 @@ module.exports = buildSchema(`
         createdEvent: [Event!]
       }
 
+      type AuthData {
+        userId: ID!
+        token: String!
+        tokenExpiration: Int!
+      }
+
       input EventInput {
         title: String!
         description: String!
@@ -39,6 +45,7 @@ module.exports = buildSchema(`
       type RootQuery {
         events: [Event!]!
         bookings: [Booking!]! 
+        login(email: String!, password: String!): AuthData!
       }
 
       type RootMutation {
