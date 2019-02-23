fd3b88da13eabb55500faa25dbc4150d631bee15
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-02-23 16:39:14 +1100

    [Maxi-Flutter] Products Stateful Widget Added

---
+++ flutter/maximillian_flutter/lib/main.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ['Food Tester'];
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Easy List'),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  child: Text('Add Product'),
+                  onPressed: () {
+                    setState(() {
+                      _products.add('Advanced food tester');
+                    });
+                  },
+                ),
+              ),
+              Column(
+                children: _products
+                    .map(
+                      (element) => Card(
+                            child: Column(
+                              children: <Widget>[
+                                Image.asset('assets/food.png'),
+                                Text(element)
+                              ],
+                            ),
+                          ),
+                    )
+                    .toList(),
+              )
+            ],
+          )),
+    );
+  }
+}
