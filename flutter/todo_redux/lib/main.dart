f37fc34b933124dad095a9cd782f89f81687e6d9
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-02-27 22:20:17 +1100

    [Todo-Redux-Flutter] Add, remove and remove all done

--- flutter/todo_redux/lib/main.dart
+++ flutter/todo_redux/lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+
+import 'package:todo_redux/models/modals.dart';
+import 'package:todo_redux/redux/actions.dart';
+import 'package:todo_redux/redux/reducers.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData.dark(),
-      home: MyHomePage(),
+    final Store<AppState> store = Store<AppState>(
+      appStateReducer,
+      initialState: AppState.initialState(),
+    );
+
+    return StoreProvider<AppState>(
+      store: store,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData.dark(),
+        home: MyHomePage(),
+      ),
     );
   }
 }
@@ -20,7 +35,104 @@ class MyHomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Redux Items"),
       ),
-      body: Container(),
+      body: StoreConnector<AppState, _ViewModel>(
+        converter: (Store<AppState> store) => _ViewModel.create(store),
+        builder: (BuildContext context, _ViewModel viewModel) => Column(
+              children: <Widget>[
+                AddItemWidget(viewModel),
+                Expanded(
+                  child: ItemListWidget(viewModel),
+                ),
+                RemoveItemsButton(viewModel)
+              ],
+            ),
+      ),
     );
+  }
+}
+
+class RemoveItemsButton extends StatelessWidget {
+  final _ViewModel model;
+  RemoveItemsButton(this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    return RaisedButton(
+      child: Text('Remove All'),
+      onPressed: () => model.onRemoveItems(),
+    );
+  }
+}
+
+class ItemListWidget extends StatelessWidget {
+  final _ViewModel model;
+  ItemListWidget(this.model);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: model.items
+          .map((Item item) => ListTile(
+                title: Text(item.body),
+                leading: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: model.onRemoveItem(item),
+                ),
+              ))
+          .toList(),
+    );
+  }
+}
+
+class AddItemWidget extends StatefulWidget {
+  final _ViewModel model;
+
+  AddItemWidget(this.model);
+  @override
+  _AddItemWidgetState createState() => _AddItemWidgetState();
+}
+
+class _AddItemWidgetState extends State<AddItemWidget> {
+  final TextEditingController controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(hintText: 'Add an Item'),
+      onSubmitted: (String s) {
+        widget.model.onAddItem(s);
+        controller.text = '';
+      },
+    );
+  }
+}
+
+class _ViewModel {
+  final List<Item> items;
+  final Function(String) onAddItem;
+  final Function(Item) onRemoveItem;
+  final Function() onRemoveItems;
+
+  _ViewModel(
+      {this.items, this.onAddItem, this.onRemoveItem, this.onRemoveItems});
+
+  factory _ViewModel.create(Store<AppState> store) {
+    _onAddItems(String body) {
+      store.dispatch(AddItemAction(body));
+    }
+
+    _onRemoveItem(Item item) {
+      store.dispatch(RemoveItemAction(item: item));
+    }
+
+    _onRemoveItems() {
+      store.dispatch(RemoveItemAction());
+    }
+
+    return _ViewModel(
+        items: store.state.items,
+        onAddItem: _onAddItems,
+        onRemoveItem: _onRemoveItem,
+        onRemoveItems: _onRemoveItems);
   }
 }
