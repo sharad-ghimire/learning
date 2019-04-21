61eba366ab5af58604517a70fb0b8767c0733e01
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-04-21 21:31:17 +1000

    [hotelmgmt] Angular app - Components added

---
+++ javascript/angular/hotelmgmt/src/app/accomodation/accomodation.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-accomodation',
+  templateUrl: './accomodation.component.html',
+  styleUrls: ['./accomodation.component.css']
+})
+export class AccomodationComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ javascript/angular/hotelmgmt/src/app/material.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { MatBadgeModule, MatButtonModule } from '@angular/material';
+
+@NgModule({
+  imports: [MatBadgeModule, MatButtonModule],
+  exports: [MatBadgeModule, MatButtonModule]
+})
+export class MaterialModule {}
