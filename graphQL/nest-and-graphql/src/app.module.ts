f8f077c2b9dbe13a8d981c3eb523fdc9cb0e7384
Sharad Ghimire <sharadghimire5551@gmail.com>
2019-04-24 22:46:07 +1000

    [Nest Js] DONE

--- graphQL/nest-and-graphql/src/app.module.ts
+++ graphQL/nest-and-graphql/src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
@@ -10,6 +12,7 @@ import { CatsModule } from './cats/cats.module';
     GraphQLModule.forRoot({
       autoSchemaFile: 'schema.gql',
     }),
+    MongooseModule.forRoot('mongodb://localhost/nestgraphql'),
   ],
   controllers: [AppController],
   providers: [AppService],
--- graphQL/nest-and-graphql/src/cats/cats.resolver.ts
+++ graphQL/nest-and-graphql/src/cats/cats.resolver.ts
@@ -1,20 +1,19 @@
-import {
-  Resolver,
-  Query,
-  Args,
-  ResolveProperty,
-  Parent,
-} from '@nestjs/graphql';
+import { Resolver, Query, Args, Parent, Mutation } from '@nestjs/graphql';
+import { CatsService } from './cats.service';
+import { CreateCatDto } from './dto/creat-cat.dto';
+import { CatInput } from './inputs/cat.input';
 
 @Resolver()
 export class CatsResolver {
-  //   constructor(
-  //     private readonly authorsService: AuthorsService,
-  //     private readonly postService: PostsService,
-  //   ) {}
+  constructor(private readonly catsService: CatsService) {}
 
-  @Query(() => String)
-  async hello() {
-    return 'hello';
+  @Query(() => [CreateCatDto])
+  async cats() {
+    return this.catsService.findAll();
+  }
+
+  @Mutation(() => CreateCatDto)
+  async createCat(@Args('input') input: CatInput) {
+    return this.catsService.create(input);
   }
 }
